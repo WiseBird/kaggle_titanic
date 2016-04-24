b3b3a094efbf7a876009da47fdcdc493eccc81bd
Sergey <sokolcjrjk@gmail.com>
2016-04-24 09:04:34 +0300

    AUC for caret

--- temp.r
+++ temp.r
@@ -18,6 +18,28 @@ source("approachs.base.R")
 source("approachs.regression.R")
 source("approachs.manual.R")
 
+calcAuc <- function(training, testing = training) {
+  fit <- train(Survived ~ ., data = training, method = "glm")
+  print(getTrainPerf(fit))
+  
+  predProb <- caret::predict.train(fit, newdata = testing, type = "prob")
+  predRaw <- caret::predict.train(fit, newdata = testing, type = "raw")
+  
+  print(confusionMatrix(predRaw, testing$Survived))
+  
+  roc <- roc(testing$Survived,
+             predProb[,1],
+             levels = rev(testing$Survived))
+  print(roc)
+  
+  plot(roc, print.thres = c(.5), type = "S",
+       print.thres.pattern = "%.3f (Spec = %.2f, Sens = %.2f)",
+       print.thres.cex = .8,
+       legacy.axes = TRUE)
+}
+
+
+
 
 # Loading data ------------------------------------------------------------
 
@@ -25,37 +47,16 @@ titanic <- read.titanic()
 titanic <- regression.simpliest$transform(titanic)
 split.res <- split.test.train(titanic)
 
-glmTitanic <- train(Survived ~ ., data = titanic, method = "glm")
-getTrainPerf(glmTitanic)
-
-glmTraining <- train(Survived ~ ., data = split.res$training, method = "glm")
-getTrainPerf(glmTraining)
-
-predProbTitanic <- caret::predict.train(glmTitanic, newdata = titanic, type = "prob")
-predRawTitanic <- caret::predict.train(glmTitanic, newdata = titanic, type = "raw")
-
-confusionMatrix(predRawTitanic, titanic$Survived)
+calcAuc(titanic)
+calcAuc(split.res$training, split.res$testing)
 
 
 
 
-predictions <- regression.simpliest$predict(split.res$training, split.res$testing)
-predictions
 
-roc0 <- roc(split.res$testing$Survived,
-            predictions,
-            levels = rev(levels(split.res$testing$Survived)))
-roc0
 
-plot(roc0, print.thres = c(.5), type = "S",
-     print.thres.pattern = "%.3f (Spec = %.2f, Sens = %.2f)",
-     print.thres.cex = .8,
-     legacy.axes = TRUE)
 
-#confusionMatrix(predictions, split.res$testing$Survived)
-confusionMatrix(ifelse(predictions < 0.5, "1", "0"), split.res$testing$Survived)
 
-pred <- prediction(as.numeric(predictions), split.res$testing$Survived)
-auc <- performance(pred,"auc")
-auc
-score.classifier(split.res, regression.simpliest)
+
+
+
